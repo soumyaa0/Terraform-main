d48ff5906b001a9e88b3716e7d9b64c20f6c6e03
soumyaa0 <141251450+soumyaa0@users.noreply.github.com>
2024-11-26 14:14:19 +0530

    Update main.tf

--- day-02-basic_ec2/main.tf
+++ day-02-basic_ec2/main.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "dev" {
     ami = var.ami
     instance_type = var.instance_type
-    subnet_id = "subnet-0f1500a3b8638b727"
+    # subnet_id = "subnet-0f1500a3b8638b727"
     key_name = var.key_name
     # vpc_security_group_ids = [ aws_security_group.allow_traffic.id ]
     tags = {
